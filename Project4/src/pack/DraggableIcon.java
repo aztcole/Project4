1131b7b92567cf00712f480f1edf5891ba37ef8e
Tyler Cole <aztcole@gmail.com>
2018-07-03 23:30:27 -0700

    Draggable Icon fully implemented
    All the methods and implementations of Draggable Icon have been added.

--- Project4/src/pack/DraggableIcon.java
+++ Project4/src/pack/DraggableIcon.java
@@ -1,22 +1,56 @@
+// Code: DraggableIcon.java
+// Lead Contributor: Tyler Cole
+// Description: This class is the parent class for all draggable icons. It allows a user to drag a JLabel
+//				and drop it into draw panel two;
+
 package pack;
 
 import java.awt.Point;
-import java.awt.dnd.DragGestureEvent;
-import java.awt.dnd.DragGestureListener;
-import java.awt.dnd.DragSourceDragEvent;
-import java.awt.dnd.DragSourceDropEvent;
-import java.awt.dnd.DragSourceEvent;
-import java.awt.dnd.DragSourceListener;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.dnd.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class DraggableIcon extends JLabel implements DragGestureListener, DragSourceListener {
 	
 	private static final long serialVersionUID = 1L;
 	
+	DragSource dSource;
+	public String iconClass;
+	private String imgSource;
+	public String persistantName;
+	
+	
 	// constructor
-	public DraggableIcon(String ImageSource, boolean draggable, String s)
+	public DraggableIcon(String ImageSource, boolean draggable, String iconType)
 	{
 		
+		iconClass = iconType;
+		setImageSource(ImageSource);
+		
+		
+		// Create Drag and Drop listener if specified as draggable 
+		if(draggable)
+		{
+			dSource = new DragSource();
+			dSource.createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_MOVE, this);
+		}
+		
+		
+		// set the icon image, and check for IO exceptions
+		try
+		{
+			this.setIcon(new ImageIcon(ImageIO.read(new File(imgSource))));
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		this.setText("yo yo yo");
 	}
 	
 	
@@ -26,9 +60,21 @@ public class DraggableIcon extends JLabel implements DragGestureListener, DragSo
 		return this.getLocation();
 	}
 	
+	
+	// setter for the image source
+	public void setImageSource(String srcImg)
+	{
+		imgSource = srcImg;
+	}
 
+	
 	@Override
-	public void dragGestureRecognized(DragGestureEvent arg0) {}
+	public void dragGestureRecognized(DragGestureEvent dge) 
+	{
+		Transferable txfr = new StringSelection(imgSource);
+		dSource.startDrag(dge, DragSource.DefaultCopyDrop, txfr, this);
+	}
+	
 	
 	@Override
 	public void dragDropEnd(DragSourceDropEvent arg0) {}
