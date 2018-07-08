64a3d656b28a01282ea4d6a878975ff74aa0e842
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-07-07 22:00:51 -0700

    singleton works

--- Project4/src/pack/QuestionPool.java
+++ Project4/src/pack/QuestionPool.java
@@ -7,18 +7,30 @@ import java.io.IOException;
 public class QuestionPool {
 	private static QuestionPool firstInstance = null;
 	ArrayList<ArrayList<String>> questions = new ArrayList<ArrayList<String>>();
+	ArrayList<String> qProperties; 
 	
 
 	private QuestionPool() throws IOException {
 		FileReader file = new FileReader("resources\\InputFile.txt");
 		BufferedReader reader = new BufferedReader(file);
-		String before;
-		String after;
 		String getLine;
 		try {
 			while((getLine = reader.readLine()) != null) {
-				if(getLine.contains("qBegin") || getLine.contains("//")) {continue;}
-				System.out.println(getLine);
+				if(getLine.contains("qBegin") || getLine == "\n"){continue;}
+				if(getLine.contains("//")) {
+					qProperties = new ArrayList<String>();
+					continue;
+					} //skip the question number and begin
+				if(getLine.contains("qEnd") == false) {
+					qProperties.add(getLine);
+				}
+				else if(getLine.contains("qEnd")) {
+					questions.add(qProperties);
+					continue;}
+				
+			}
+			for(int i = 0; i< questions.size(); i++) {
+				System.out.println(questions.get(i) + "\n");
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
